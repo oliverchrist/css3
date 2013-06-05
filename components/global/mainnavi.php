faf74242f71c13341ac0f392c6a55459d5ea4ee0
ochrist <oliver.christ@mediaman.de>
2013-06-05 18:21:07 +0200

    added mainnavi

--- components/global/mainnavi.php
+++ components/global/mainnavi.php
@@ -1,9 +1,8 @@
 <?php
-$jsonfile = file_get_contents($basePath . 'config/subnavi.json');
+$jsonfile = file_get_contents($basePath . 'config/mainnavi.json');
 $jsonarray = json_decode($jsonfile);
-
 echo '<nav class="mainnavi">';
-    foreach ($jsonarray->subnavi as $key => $value) {
+    foreach ($jsonarray->mainnavi as $key => $value) {
         echo "<a href=\"$value->href\" target=\"$value->target\">$value->label</a>";
     }
 echo '</nav>';
