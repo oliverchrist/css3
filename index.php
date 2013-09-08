b50f21327a02103973e856f23218398acbadaa29
Oliver Christ <oliver.christ@mediaman.de>
2013-09-08 23:37:20 +0200

    some changes, can't remember

--- index.php
+++ index.php
@@ -6,17 +6,17 @@ $basePath = '';
     <head>
         <?php include 'components/global/head.php'; ?>
     </head>
-    <body>
+    <body id="home">
         <div class="website">
             <?php include 'components/global/header.php'; ?>
             <div id="main">
                 <?php include 'components/global/subnavi.php'; ?>
                 <section class="content">
-                    <h1>Ollis css3 Spielwiese</h1>
+                    <h1>Website von Oliver Christ</h1>
                     <p>Hier entsteht meine css3 Spielwiese, ohne Rücksicht auf veraltete Browser</p>
                     <p>Zugleich ist dies ein Experiment zur agilen Erstellung einer Website nach einem Prinzip, welches ich KIfSfS nennen möchte</p>
                     <p>KIfSfS steht für "keep it fantastic simple and fantastic stupid"</p>
-                    <p>Das Prinzip soll es dem Konzept ermöglichen selbst Texte in Webseiten einzupflegen. Im mindesten soll es dem Service Team die Content Eingabe erleichtern, welche oft durch ein CMS nur erschwert wird.</p>
+                    <p>Das Prinzip soll es dem Gewerk Konzept ermöglichen selbst Texte in Webseiten einzupflegen. Im mindesten soll es dem Service Team die Content Eingabe erleichtern, welche oft durch ein CMS nur erschwert wird.</p>
                 </section>
                 <aside class="teaser"></aside>
                 <div class="clear"></div>
