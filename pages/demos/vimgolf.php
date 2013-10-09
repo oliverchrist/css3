544ed9ec6ce3b457a5af61ebb8d19b2212ef779b
Oliver Christ <oliver.christ@mediaman.de>
2013-10-10 00:17:51 +0200

    added vimgolf page

---
+++ pages/demos/vimgolf.php
@@ -0,0 +1,31 @@
+<?php
+$basePath = $_SERVER['DOCUMENT_ROOT'];
+if(substr($basePath, -1, 1) != '/') { $basePath .= '/'; }
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include $basePath . 'components/global/head.php'; ?>
+        <script type="text/javascript" src="/assets/javascript/vimgolf.js"></script>
+    </head>
+    <body>
+        <div class="website">
+            <?php include $basePath . 'components/global/header.php'; ?>
+            <div id="main">
+                <?php include $basePath . 'components/global/subnavi.php'; ?>
+                <section class="content">
+                    <h1>Vimgolf Rank</h1>
+                    <div id="challenges">
+                        <?php
+                        $homepage = file_get_contents('http://www.vimgolf.com/');
+                        echo $homepage;
+                        ?>
+                    </div>
+                </section>
+                <aside class="teaser"></aside>
+                <div class="clear"></div>
+            </div>
+            <?php include $basePath . 'components/global/footer.php'; ?>
+        </div>
+    </body>
+</html>
