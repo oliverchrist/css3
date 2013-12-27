6dc4648c1984a427148b93f826694958a2d84756
ochrist <oliver.christ@mediaman.de>
2013-12-28 00:42:04 +0100

    bilder aufräumen

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,47 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         imagemin: { // Task
-            static: { // Target
-                options: { // Target options
-                    optimizationLevel: 3
-                },
-                files: { // Dictionary of files
-                    'pages/reviews/beer/images/altes.JPG': 'pages/reviews/beer/images/altes2.JPG', // 'destination': 'source'
+            static: {
+                files: [{
+                    expand: true,
+                    cwd: 'pages/reviews/beer/images/',
+                    src: '*.{png,jpg,jpeg,svg,JPG}',
+                    dest: 'pages/reviews/beer/images3/'
+                }]
+            }
+        },
+        multiresize: {
+            icon: {
+                src: 'pages/reviews/beer/images/altes.JPG',
+                dest: ['pages/reviews/beer/images/altes2.JPG', 'pages/reviews/beer/images/altes3.JPG'],
+                destSizes: ['236x709', '200x700']
+            }
+        },
+        "imagemagick-hisrc": {
+            dev: {
+                files: "**/*-2x.jpg",
+                suffix: ["-2x", "-1x", "-low"],
+            }
+        },
+        "imagemagick-resize": {
+            dev: {
+                from: 'pages/reviews/beer/images/',
+                to: 'pages/reviews/beer/images/resized/',
+                files: '*.JPG',
+                props: {
+                    width: 760
                 }
-            },
+            }
+        },
+        "imagemagick-convert": {
+            dev: {
+                args: ['pages/reviews/beer/images3/*.JPG', '-crop', '300x610+256+0', 'pages/reviews/beer/images4/*.JPG']
+            }
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-imagemagick');
 
-    grunt.registerTask('default', ['imagemin']);
+    grunt.registerTask('default', ['imagemagick-resize']);
+    //grunt.registerTask('default', ['imagemagick-convert']);
 };
