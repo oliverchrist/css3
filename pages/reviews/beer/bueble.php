7e566b0b19abe8b8b9b8c2d06a45787b3908779c
ochrist <oliver.christ@mediaman.de>
2013-12-28 02:09:41 +0100

    added beers and changed path in pages to resized beers

--- pages/reviews/beer/bueble.php
+++ pages/reviews/beer/bueble.php
@@ -15,7 +15,7 @@ if(substr($basePath, -1, 1) != '/') { $basePath .= '/'; }
                 <section class="content">
                     <h1>Büble</h1>
                     <a class="floatLeft christbox" href="images/bueble.JPG">
-                        <img src="images/buebleThumb.JPG" alt="Büble" />
+                        <img src="images/resized/bueble.JPG" alt="Büble" />
                     </a>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </section>
